433ce984fd0aa1063fbda07c306fd47d9ec06644
David Sabalete <dsabalete@gmail.com>
2017-11-30 00:13:54 +0100

    Connect AddEntry to Redux

--- App.js
+++ App.js
@@ -1,10 +1,5 @@
 import React from 'react'
-import {
-	View,
-	Text,
-	StyleSheet,
-	Slider
-} from 'react-native'
+import { View } from 'react-native'
 import AddEntry from './components/AddEntry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
@@ -22,25 +17,3 @@ export default class App extends React.Component {
 		)
 	}
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		marginLeft: 10,
-		marginRight: 10,
-		alignItems: 'stretch',
-		justifyContent: 'center'
-	},
-	btn: {
-		backgroundColor: '#E53224',
-		padding: 10,
-		paddingLeft: 50,
-		paddingRight: 50,
-		justifyContent: 'center',
-		alignItems: 'center',
-		borderRadius: 5
-	},
-	btnText: {
-		color: '#fff'
-	}
-})
