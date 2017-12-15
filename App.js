777cc4aa9dc7fcf5486441c67be7aa284ac11995
David Sabalete <dsabalete@gmail.com>
2017-12-15 23:14:47 +0100

    Implement askPermission in Live component

--- App.js
+++ App.js
@@ -43,24 +43,24 @@ const Tabs = TabNavigator({
 		}		
 	}
 }, {
-		navigationOptions: {
-			header: null
-		},
-		tabBarOptions: {
-			activeTintColor: Platform.OS === 'ios' ? purple : white,
-			style: {
-				height: 56,
-				backgroundColor: Platform.OS === 'ios' ? white : purple,
-				shadowColor: 'rgba(0, 0, 0, 0.24)',
-				shadowOffset: {
-					width: 0,
-					height: 3
-				},
-				shadowRadius: 6,
-				shadowOpacity: 1
-			}
+	navigationOptions: {
+		header: null
+	},
+	tabBarOptions: {
+		activeTintColor: Platform.OS === 'ios' ? purple : white,
+		style: {
+			height: 56,
+			backgroundColor: Platform.OS === 'ios' ? white : purple,
+			shadowColor: 'rgba(0, 0, 0, 0.24)',
+			shadowOffset: {
+				width: 0,
+				height: 3
+			},
+			shadowRadius: 6,
+			shadowOpacity: 1
 		}
-	})
+	}
+})
 
 const MainNavigator = StackNavigator({
 	Home: {
--- components/Live.js
+++ components/Live.js
@@ -2,17 +2,60 @@ import React, { Component } from 'react'
 import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet } from 'react-native'
 import { Foundation } from '@expo/vector-icons'
 import { purple, white } from '../utils/colors'
+import { Location, Permissions } from 'expo'
+import { calculateDirection } from '../utils/helpers'
 
 
 class Live extends Component {
     state = {
         coords: null,
-        status: 'denied',
+        status: 'undetermined',
         direction: ''
     }
 
-    askPermission = () => {
+    componentDidMount() {
+        Permissions.getAsync(Permissions.LOCATION)
+            .then(({ status }) => {
+                if (status === 'granted') {
+                    return this.setLocation()
+                }
 
+                this.setState(() => ({ status }))
+            })
+            .catch((error) => {
+                console.warn('Error getting location permission: ', error)
+
+                this.setState(() => ({ status: 'undetermined' }))
+            })
+    }
+
+    askPermission = () => {
+        Permissions.askAsync(Permissions.LOCATION)
+            .then(({ status }) => {
+                if (status === 'granted') {
+                    return this.setLocation()
+                }
+                
+                this.setState(() => ({ status }))
+            })
+            .catch((error) => console.warn('Error asking Location permission', error))
+    }
+
+    setLocation = () => {
+        Location.watchPositionAsync({
+            enableHighAccuracy: true,
+            timeInterval: 1,
+            distanceInterval: 1,
+        }, ({ coords }) => {
+            const newDirection = calculateDirection(coords.heading)
+            const { direction, bounceValue } = this.state
+
+            this.setState(() => ({
+                coords,
+                status: 'granted',
+                direction: newDirection
+            }))
+        })
     }
 
     render() {
@@ -50,9 +93,29 @@ class Live extends Component {
         }
 
         return (
-            <View>
-                <Text>Live</Text>
-                <Text>{JSON.stringify(this.state)}</Text>
+            <View style={styles.container}>
+                <View style={styles.directionContainer}>
+                    <Text style={styles.header}>You're heading</Text>
+                    <Text style={styles.direction}>{direction}</Text>
+                </View>
+                <View style={styles.metricContainer}>
+                    <View style={styles.metric}>
+                        <Text style={[styles.header, { color: white }]}>
+                            Altitud
+						</Text>
+                        <Text style={[styles.subHeader, { color: white }]}>
+                            {Math.round(coords.altitude * 3.2808)} Feet
+						</Text>
+                    </View>
+                    <View style={styles.metric}>
+                        <Text style={[styles.header, { color: white }]}>
+                            Speed
+						</Text>
+                        <Text style={[styles.subHeader, { color: white }]}>
+                            {(coords.speed * 2.2369).toFixed(1)} MPH
+						</Text>
+                    </View>
+                </View>
             </View>
         )
     }
@@ -80,7 +143,40 @@ const styles = StyleSheet.create({
     buttonText: {
         color: white,
         fontSize: 20,
-    }
+    },
+    directionContainer: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    header: {
+        fontSize: 35,
+        textAlign: 'center',
+    },
+    direction: {
+        color: purple,
+        fontSize: 120,
+        textAlign: 'center',
+    },
+    metricContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        backgroundColor: purple,
+    },
+    metric: {
+        flex: 1,
+        paddingTop: 15,
+        paddingBottom: 15,
+        backgroundColor: 'rgba(255, 255, 255, 0.1)',
+        marginTop: 20,
+        marginBottom: 20,
+        marginLeft: 10,
+        marginRight: 10,
+    },
+    subHeader: {
+        fontSize: 25,
+        textAlign: 'center',
+        marginTop: 5,
+    },
 })
 
 export default Live
